21e78cf8e21b86574d7e5aab740389419e7b1f6d
sazeed <sazeed199516@gmail.com>
2020-05-23 13:19:02 +0530

    added validations for User model

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :username, uniqueness: { case_sensitive: false }, presence: true, 
+                    length: {minimum:3, maximum:25 }
+    VALID_EMAIL_CHECK = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+                     length: {maximum:105 },
+                     format: {with: VALID_EMAIL_CHECK}
 end
