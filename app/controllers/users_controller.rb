bb578ef4856173bc5c97fc60460a6c70679ea1ae
sazeed <sazeed199516@gmail.com>
2020-05-26 18:11:10 +0530

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,28 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end 
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          redirect_to @user
+        else
+          render 'edit'
+        end
+      end
+
     def create
         @user = User.new(params.require(:user).permit(:username, :email, :password))
        if @user.save
@@ -13,6 +35,11 @@ class UsersController < ApplicationController
     else
         render 'new'
     end
+        end
+
+        private
+        def user_params
+            params.require(:user).permit(:username, :email, :password)
         end
 
 end
