d964479de341cc366abb43bd68349fc317602871
sazeed <sazeed199516@gmail.com>
2020-05-27 12:50:31 +0530

    delete user

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase}
-    has_many :articles, dependent: : destroy
+    has_many :articles, dependent: :destroy
     validates :username, uniqueness: { case_sensitive: false }, presence: true, 
                     length: {minimum:3, maximum:25 }
     VALID_EMAIL_CHECK = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
